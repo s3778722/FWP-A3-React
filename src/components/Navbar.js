1b58575dfab7cfdd444e609593c3cd6e1a2098a0
s3778722 <s3778722@student.rmit.edu.au>
2021-10-25 11:56:19 +1100

    Added React Bootstrap, Jquery and Navbar Component

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">
+            RMIT Further Web Programming A3
+          </span>
+          <span class="navbar-text">Student Id: s3778722</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
